f4634068639e18e815926149ddc2454519be8b8f
FranjeGueje <fjgj1@hotmail.com>
2026-05-16 00:19:27 +0200

    Casi casi, sin cache errornea

--- puntueitor/core/pipeline/load_steam_library.py
+++ puntueitor/core/pipeline/load_steam_library.py
@@ -62,7 +62,7 @@ def load_library(
     api_key = api_key or config.steam_api_key
     user = user or config.steam_user_id
 
-    base_path = Path(__file__).parent.parent.parent
+    base_path = Path(__file__).resolve().parents[3]
     CACHE_RESOLVERS = base_path / "cache" / "resolvers.sqlite"
     steam_selector = SteamSelector()
 
